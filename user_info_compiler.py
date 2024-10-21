9b7e6b5c90018036022aa163062bd190eabcdf21
Marian <ianlegaspi.marian@gmail.com>
2024-10-21 23:06:55 +0800

    Added a prompt for users to enter their age

--- user_info_compiler.py
+++ user_info_compiler.py
@@ -3,7 +3,19 @@ users_info = []
 while True:
    user_name = input("Enter your name: ")
    #Use while not, to repeat task until condition is met
-   while not (user_name.isalpha() and len(user_name) >= 2): #only accepts alphabet and atleast more that 2 characters long
+   #for user name
+   while not (user_name.isalpha() and len(user_name) >= 2): #only accepts alphabet and atleast more than 2 characters long
       print("Not a valid name. Enter a name that only contains alpahabet and atleast 2 letters long.")
       user_name = input("Enter your name: ")
-      break     
+   #for user age
+   user_age = int(input("Enter your age: "))  
+   while not ((user_age.is_integer()) and 0 <= int(user_age) <= 150):#only accepts integer between 0-150 
+      print("Not a valid age. Enter Age between 0 and 150.")
+      user_age = int(input("Enter your age: "))
+
+
+   
+
+   
+
+   
