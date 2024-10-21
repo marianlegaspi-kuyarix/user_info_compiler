8116ee599c1bdcb14a581cb747f05cd3e37e6a38
Marian <ianlegaspi.marian@gmail.com>
2024-10-21 22:22:30 +0800

    Accidentally deleted my code, added a prompt for users to input their name

--- user_info_compiler.py
+++ user_info_compiler.py
@@ -1,4 +1,9 @@
 #Initialize an empty list of users info
 users_info = []
-
-            
+while True:
+   user_name = input("Enter your name: ")
+   #Use while not, to repeat task until condition is met
+   while not (user_name.isalpha() and len(user_name) >= 2): #only accepts alphabet and atleast more that 2 characters long
+      print("Not a valid name. Enter a name that only contains alpahabet and atleast 2 letters long.")
+      user_name = input("Enter your name: ")
+      break     
