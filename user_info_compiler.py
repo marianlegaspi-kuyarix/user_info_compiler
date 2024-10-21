7257e620a9641c2d093ddc32ea4a42ef9cf12d36
Marian <ianlegaspi.marian@gmail.com>
2024-10-22 01:25:59 +0800

    First, fixed the format for the oldest user. So it looks neat and readable. Additionally, I opted to using try/except and if statements, so that when a string is inputted it will not break the program. lastly, I Added '.title() after the user name so it will print in title case.

--- user_info_compiler.py
+++ user_info_compiler.py
@@ -1,18 +1,24 @@
 #Initialize an empty list of users info
 users_info = []
 while True:
-   user_name = input("Enter your name: ")
+   user_name = input("Enter your name: ").title()
    #Use while not, to repeat task until condition is met
    #for user name
    while not (user_name.isalpha() and len(user_name) >= 2): #only accepts alphabet and atleast more than 2 characters long
       print("Not a valid name. Enter a name that only contains alpahabet and atleast 2 letters long.")
-      user_name = input("Enter your name: ")
-      
+      user_name = input("Enter your name: ").title()
+
    #for user age
-   user_age = int(input("Enter your age: "))  
-   while not ((user_age.is_integer()) and 0 <= int(user_age) <= 150):#only accepts integer between 0-150 
-      print("Not a valid age. Enter Age between 0 and 150.")
-      user_age = int(input("Enter your age: "))
+   while True:
+      try:
+         user_age = int(input("Enter your age: "))  
+         if 0 <= int(user_age) <= 150: #only accepts integer between 0-150
+            break
+         else:
+            print("Not a valid age. Enter Age between 0 and 150.")
+      except ValueError: #for the program to not stop when a string is entered
+         print("Not a valid age. Enter Age between 0 and 150.")
+
    #stores the info in the list  
    users_info.append({"Name": user_name, "Age": int(user_age)})
 
@@ -28,4 +34,4 @@ while True:
 #when user do not want to continue, it will display the oldest person 
 if users_info:
    oldest_user = max(users_info, key=lambda user: user["Age"])
-print(f"Oldest user: \n{oldest_user}")
+print(f"Oldest user: \nName: {oldest_user['Name']}\nAge: {oldest_user['Age']}")
